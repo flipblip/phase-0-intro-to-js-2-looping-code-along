cce4fb3a50eb5e79ba45f75b0bdd31ad3a5511dd
wickynjy@gmail.com <meow@meow>
2022-08-15 07:43:23 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+//writing cards function
+
+
+function writeCards(cards, eventName) {
+    let msgArray = [];
+    for (let i=0; i < cards.length; i++) {
+        
+        let message = `Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`
+        msgArray.push(message);
+    }
+    return msgArray;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday")
+
+// writeCards(cards);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        --number;
+    }
+}
+
